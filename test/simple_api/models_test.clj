e5c2d7d74646e2e109000ff42837bcf7773670da
rubygeek <nola@rubygeek.com>
2015-04-19 17:08:38 -0500

    finished tests

--- test/simple_api/models_test.clj
+++ test/simple_api/models_test.clj
@@ -1,9 +1,33 @@
 (ns simple-api.models-test
   (:require [clojure.test :refer :all]
-            [simple-api.models :refer :all]))
+            [simple-api.models :refer :all]
+            [schema.core :as s]))
+
+(use-fixtures :each
+  (fn [tests]
+    (add! {:name "test" :url "test.com" :source {:name "mom" :url "mom.com"}})
+    (tests)))
+
 
 (deftest test-repository
   (testing "adding recipe"
-     (let [recipe {:name "test" :url "test.com" :source {:name "mom" :url "mom.com"}}
+     (let [recipe {:name "test more" :url "testmore.com" :source {:name "mom" :url "mom.com"}}
            r (add! recipe)]
-       (is (= 1 (recipe-count))))))
+       (is (= 2 (recipe-count)))))
+
+  (testing "updating recipe"
+    (let [recipe (get-recipe 1)
+          update "updated"
+          updated-recipe (update! 1 {:name update})]
+      (is (= (:name updated-recipe) update )))) 
+ 
+  (testing "deleting recipe"
+    (let [id  (:id (get-recipe 1) )
+          deleted (delete! id)]
+      (is (nil? deleted))))
+
+  (testing "throws ExceptionInfo error when invalid data"
+    (let [recipe-data {:name "test"}]
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (add! recipe-data))))))
+
