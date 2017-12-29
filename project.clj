c5b26a716c45dec3854286b07a6be7d9778985b2
Nola Stowe <nola@rubygeek.com>
2017-12-29 09:23:56 -0600

    added reloaded

--- project.clj
+++ project.clj
@@ -1,13 +1,18 @@
 (defproject simple-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple api now with Clojure 1.9 and spec"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.2"]
-                 [metosin/ring-swagger "0.22.14"]
-                 [ring/ring-defaults "0.2.3"]]
-  :plugins [[lein-ring "0.8.13"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [compojure "1.6.0"]
+                 [metosin/ring-swagger "0.24.4"]
+                 [ring/ring-defaults "0.3.1"]
+                 [com.stuartsierra/component "0.3.2"]]
+  :plugins [[lein-ring "0.12.2"]]
   :ring {:handler simple-api.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+ 
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
+                                  [com.stuartsierra/component.repl "0.2.0"]
+                                  [javax.servlet/servlet-api "2.5"]
+                                  [ring-mock "0.1.5"]]
+                   :source-paths ["dev"]}})
+
+
